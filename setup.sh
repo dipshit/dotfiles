178cd134ed73eea1463cce4c337029700efe10e5
The Cooler Kirin <52986010+dipshit@users.noreply.github.com>
2020-04-09 11:23:26 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Varibles
+backup=~/.dotfiles_backup
+dir="$(pwd)"
+dotfiles="
+.tmux.conf
+.zshrc
+.gitconfig
+.gitignore_global
+.config/nvim
+.config/alacritty"
+
+# Backup
+mkdir -p $backup
+echo "Backup dir: $backup"
+
+# Setup config
+mkdir ~/.config
+
+for dotfile in $dotfiles; do
+  # move backups
+  [[ -h ~/$dotfile ]] && unlink ~/"$dotfile"
+	if [[ -f ~/$dotfile ]]; then
+    mv ~/"$dotfile" "$backup"/"$dotfile"
+    echo "Current $dotfile moved to: $backup/$dotfile"
+  fi
+
+  # make symlinks
+  if [[ -d $dotfile ]]; then
+    ln -sF "$dir"/"$dotfile" ~/"$(dirname "$dotfile")"
+  else
+    ln -sF "$dir"/"$dotfile" ~/"$dotfile"
+  fi
+	echo "Symbloic link for ~/$dotfile"
+done
